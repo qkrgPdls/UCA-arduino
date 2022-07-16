c5d1de511ee01393f0df5a6a87a44c7f355a17a1
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-16 18:12:01 +0900

    led 아날로그 점차 밝아지고 꺼지기

--- analog LED1/analog LED1.ino
+++ analog LED1/analog LED1.ino
@@ -1,9 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
+  //pinMode(7, OUTPUT);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //digitalWrite(7, HIGH);
+  analogWrite(3, 200);
+  for(int i=0;i<256;i++){
+    analogWrite(3, i);
+    delay(10);
+  }
+  for(int i=255;i>0;i--){
+    analogWrite(3, i);
+    delay(10);
+  }
 
 }
