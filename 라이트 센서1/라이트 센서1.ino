a1426c14406946dfa8201fba6b8cd9a7c7975659
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-16 18:50:49 +0900

    led 아날로그 시리얼

--- 라이트 센서1/라이트 센서1.ino
+++ 라이트 센서1/라이트 센서1.ino
@@ -1,11 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  int lightValue = analogRead(A0);
+  Serial.println(lightValue);
+  if(lightValue < 400){
+    digitalWrite(2, HIGH);
+  }
 
 }
