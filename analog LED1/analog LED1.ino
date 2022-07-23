125f7f341f5af2bdc222126bd4da04b2b703fcf7
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-23 16:42:24 +0900

    버튼1

--- analog LED1/analog LED1.ino
+++ analog LED1/analog LED1.ino
@@ -1,21 +1,21 @@
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(7, OUTPUT);
-
+  Serial.begin(9600);
+pinMode(3,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //digitalWrite(7, HIGH);
-  analogWrite(3, 200);
-  for(int i=0;i<256;i++){
-    analogWrite(3, i);
-    delay(10);
-  }
-  for(int i=255;i>0;i--){
-    analogWrite(3, i);
-    delay(10);
-  }
+ 
+ 
 
+ 
+
+   int lightValue = analogRead(A0);
+   Serial.println(lightValue);
+   if(lightValue < 500){
+    digitalWrite(3,HIGH);
+   }
+   if(lightValue > 500){
+    digitalWrite(3,LOW);
+   }
 }
