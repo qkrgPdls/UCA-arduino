bdfeda2afcc458551725b89aae9188460260b7e5
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-23 17:36:25 +0900

    버튼 따로 2개 불 켜기

--- bt1/bt1.ino
+++ bt1/bt1.ino
@@ -1,9 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(4, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int buttonV = digitalRead(4);
+  int buttonVa = digitalRead(7);
+  if(buttonV==0){
+    digitalWrite(2, HIGH);
+  }
+  else{
+    digitalWrite(2, LOW);
+  }
+  if(buttonVa==0){
+    digitalWrite(8, HIGH);
+  }
+  else{
+    digitalWrite(8, LOW);
+  }
 
 }
