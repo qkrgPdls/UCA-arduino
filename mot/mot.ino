8065f1e8bc0f01084cc196626b9ae0725d30d9c3
josiuuuuu <joeyjoeyhi5ivegood@outlook.kr>
2022-07-30 17:47:44 +0900

    조도 모터 다이오드

---
+++ mot/mot.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+Servo servo;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(9);
+  Serial.begin(9600);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);*/
+  int light = digitalRead(7);
+  Serial.println(light);
+  if(light < 300){
+    for(int i = 0;i < 181;i++){
+      servo.write(i);
+     delay(10);
+     
+     }
+    for(int j = 180;j > -1;j--){
+      servo.write(j);
+     delay(10);
+      
+    }
+    digitalWrite(4, HIGH);
+    delay(100);
+  }
+  
+}
